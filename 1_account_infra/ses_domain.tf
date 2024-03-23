6343e9e649ed92d944007896f3e521050006f32b
Chase 李 <chase@402.wtf>
2024-03-22 23:33:33 -0500

    Adding ses identities.

---
+++ 1_account_infra/ses_domain.tf
@@ -0,0 +1,4 @@
+resource "aws_ses_domain_identity" "domains" {
+  for_each = var.ses_domains
+  domain = each.key
+}
--- 1_account_infra/variables.tf
+++ 1_account_infra/variables.tf
@@ -3,3 +3,9 @@ variable "prefix" {
   default     = "402-wtf"
   description = "The prefix to use for created resources"
 }
+
+variable "ses_domains" {
+  type        = list(string)
+  default     = []
+  description = "A list of domain names to use as SES identities"
+}
