71893bbf8c235f6bc2e583bd211c967bc03f6ee7
Chase 李 <chase@402.wtf>
2024-03-23 05:09:26 -0500

    Adding dkim generation

--- 1_account_infra/ses_domain.tf
+++ 1_account_infra/ses_domain.tf
@@ -2,3 +2,8 @@ resource "aws_ses_domain_identity" "domains" {
   for_each = var.ses_domains
   domain   = each.key
 }
+
+resource "aws_ses_domain_dkim" "domains" {
+  for_each = var.ses_domains
+  domain = aws_ses_domain_identity.domains[each.key].domain
+}
