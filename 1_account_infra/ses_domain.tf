b0158abc8b30c9997c6f464cebca99675705a789
Chase 李 <me@chase.ninja>
2024-03-28 02:44:32 -0500

    👮Adding dkim generation (#2)
    * Adding dkim generation
    
    * Adding Cloudflare provider to handle DNS record management.
    
    * Adding Cloudflare provider
    
    * Adding cloudflare records for SES domains
    
    * Adding comment to DNS records
    
    * Updating comment
    
    ---------
    
    Co-authored-by: Chase 李 <chase@402.wtf>

--- 1_account_infra/ses_domain.tf
+++ 1_account_infra/ses_domain.tf
@@ -2,3 +2,33 @@ resource "aws_ses_domain_identity" "domains" {
   for_each = var.ses_domains
   domain   = each.key
 }
+
+resource "aws_ses_domain_dkim" "domains" {
+  for_each = var.ses_domains
+  domain   = aws_ses_domain_identity.domains[each.key].domain
+}
+
+data "cloudflare_zone" "dns_zone" {
+  for_each = var.ses_domains
+  name     = each.key
+}
+
+locals {
+  dkim_tokens = flatten([for domain in aws_ses_domain_dkim.domains : [
+    for token in domain.dkim_tokens : {
+      "domain" = domain.domain
+      "token"  = token
+    }
+    ]
+  ])
+}
+
+resource "cloudflare_record" "dkim_records" {
+  count   = length(local.dkim_tokens)
+  zone_id = data.cloudflare_zone.dns_zone[local.dkim_tokens[count.index].domain].id
+  name    = "${local.dkim_tokens[count.index].token}._domainkey"
+  value   = "${local.dkim_tokens[count.index].token}.dkim.amazonses.com"
+  type    = "CNAME"
+  ttl     = 300
+  comment = "AWS SES - ${local.dkim_tokens[count.index].domain} - Managed by OpenTofu"
+}
